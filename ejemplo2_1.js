2512eb187f12da257e2c4b69a3273d692e9b79e6
Diana Lopez Vega <dianateresalopezvega@gmail.com>
2025-05-06 20:22:40 -0600

    merge remote changes into main

---
+++ ejemplo2_1.js
@@ -0,0 +1,11 @@
+//programa para calcular el area de un area grande si el area es ayor a 10 bxh/2
+
+//declaracion de las variables d entrada
+var base = prompt("ingresar valor de base"); 
+var altura = prompt("ingresar valor de altura"); 
+
+// proceso a realizar 
+var area = (base * altura)/2; 
+
+//imprimir la salida o resultado solicitado
+console.log("el area del triangulo es : ",area );
