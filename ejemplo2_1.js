27d000a902e9326671cc830360d430a9b789e457
Diana Lopez Vega <dianateresalopezvega@gmail.com>
2025-05-06 20:24:28 -0600

    guaradndo cambios locales antes del rebase

--- ejemplo2_1.js
+++ ejemplo2_1.js
@@ -7,5 +7,8 @@ var altura = prompt("ingresar valor de altura");
 // proceso a realizar 
 var area = (base * altura)/2; 
 
+//aplicarse aqi el condicional
+if (area>10) alert("el area del triangulo es grande")
+
 //imprimir la salida o resultado solicitado
 console.log("el area del triangulo es : ",area );
